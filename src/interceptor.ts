f47cf6fae1fa50945390fe77d411c1f5e80d2ab9
PinkiePieStyle <svenw1980@gmail.com>
2020-01-14 15:28:48 +0100

    sync

--- src/interceptor.ts
+++ src/interceptor.ts
@@ -1,5 +1,5 @@
 /**
-    Dwarf - Copyright (C) 2019 Giovanni Rocca (iGio90)
+    Dwarf - Copyright (C) 2018-2020 Giovanni Rocca (iGio90)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>
 **/
+
 
 
 import { ThreadContext } from "./thread_context";
