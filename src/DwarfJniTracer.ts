c57eb30247b37f98646b0c268f37e69f570c54d1
PinkiePieStyle <svenw1980@gmail.com>
2021-02-13 13:56:26 +0100

    jni tracer

---
+++ src/DwarfJniTracer.ts
@@ -0,0 +1,66 @@
+/*
+    Dwarf - Copyright (C) 2018-2021 Giovanni Rocca (iGio90)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+//based on
+//https://github.com/mapbox/jni.hpp/blob/master/test/openjdk/jni.h
+//https://docs.oracle.com/javase/8/docs/technotes/guides/jni/spec/functions.html
+import { JNI_Functions } from "./consts";
+
+export class DwarfJniTracer {
+    protected _vm_env = Java.vm.getEnv();
+    private _listeners: Array<InvocationListener>;
+
+    constructor() {
+        this._listeners = new Array<InvocationListener>(JNI_Functions.GetObjectRefType);
+        for (let i = 0; i < this._listeners.length; i++) {
+            this._listeners[i] = null;
+        }
+    }
+
+    private _getNativeFuncPtr = (index: number): NativePointer => {
+        if (index <= JNI_Functions.reserved3 || index > JNI_Functions.GetObjectRefType) {
+            throw new Error("JNITracer: Invalid function!");
+        }
+        return this._vm_env.handle
+            .readPointer()
+            .add(index * Process.pointerSize)
+            .readPointer();
+    };
+
+    traceFunction = (fncIdx: JNI_Functions) => {
+        if (fncIdx <= JNI_Functions.reserved3 || fncIdx > JNI_Functions.GetObjectRefType) {
+            throw new Error("JNITracer: Invalid function!");
+        }
+
+        Interceptor.attach(this._getNativeFuncPtr(fncIdx), {
+            onEnter: function (args) {
+                //TODO: define tpl
+            },
+            onLeave: function (retval) {},
+        });
+    };
+
+    removeTrace = (fncIdx: JNI_Functions) => {
+        if (fncIdx <= JNI_Functions.reserved3 || fncIdx > JNI_Functions.GetObjectRefType) {
+            throw new Error("JNITracer: Invalid function!");
+        }
+        if (this._listeners[fncIdx] !== null) {
+            this._listeners[fncIdx].detach();
+            this._listeners[fncIdx] = null;
+        }
+    };
+}
--- src/consts.ts
+++ src/consts.ts
@@ -19,38 +19,38 @@ export const enum DwarfLogType {
     PRINT = 1,
     DEBUG = 2,
     WARNING = 3,
-    ERROR = 4
+    ERROR = 4,
 }
 
 export const enum DwarfPlatform {
     OS_WINDOWS = 1,
     OS_DARWIN = 2,
     OS_LINUX = 3,
-    OS_QNX = 4
+    OS_QNX = 4,
 }
 
 export const enum DwarfArch {
     ARCH_X86 = 1,
     ARCH_X64 = 2,
     ARCH_ARM = 3,
-    ARCH_ARM64 = 4
+    ARCH_ARM64 = 4,
 }
 
 export const enum DwarfBits {
     BITS_32 = 1,
-    BITS_64 = 2
+    BITS_64 = 2,
 }
 
 export const enum DwarfEndian {
     LITTLE_ENDIAN = 1,
-    BIG_ENDIAN = 2
+    BIG_ENDIAN = 2,
 }
 
 export const enum DwarfSessionType {
     ANDROID = 1,
     LOCAL = 2,
     IOS = 3,
-    REMOTE = 4
+    REMOTE = 4,
 }
 
 export const enum DwarfHookType {
@@ -59,20 +59,20 @@ export const enum DwarfHookType {
     OBJC = 3,
     MEMORY = 4,
     MODULE_LOAD = 5,
-    CLASS_LOAD = 6
+    CLASS_LOAD = 6,
 }
 
 export const enum DwarfMemoryAccessType {
     READ = 1,
     WRITE = 2,
-    EXECUTE = 4
+    EXECUTE = 4,
 }
 
 export const enum DwarfHaltReason {
     INITIAL_CONTEXT = 1,
     BP_INITIALIZATION = 2,
     BREAKPOINT = 3,
-    STEP = 4
+    STEP = 4,
 }
 
 export const enum DwarfDataDisplayType {
@@ -80,5 +80,280 @@ export const enum DwarfDataDisplayType {
     JSON = 2,
     HEX = 3,
     DISASM = 4,
-    SQLITE3 = 5
+    SQLITE3 = 5,
+}
+
+export const enum JNI_Functions {
+    reserved0,
+    reserved1,
+    reserved2,
+    reserved3,
+
+    GetVersion,
+
+    DefineClass,
+    FindClass,
+
+    FromReflectedMethod,
+    FromReflectedField,
+    ToReflectedMethod,
+
+    GetSuperclass,
+    IsAssignableFrom,
+
+    ToReflectedField,
+
+    Throw,
+    ThrowNew,
+    ExceptionOccurred,
+    ExceptionDescribe,
+    ExceptionClear,
+    FatalError,
+
+    PushLocalFrame,
+    PopLocalFrame,
+
+    NewGlobalRef,
+    DeleteGlobalRef,
+    DeleteLocalRef,
+    IsSameObject,
+    NewLocalRef,
+    EnsureLocalCapacity,
+
+    AllocObject,
+    NewObject,
+    NewObjectV,
+    NewObjectA,
+
+    GetObjectClass,
+    IsInstanceOf,
+
+    GetMethodID,
+
+    CallObjectMethod,
+    CallObjectMethodV,
+    CallObjectMethodA,
+    CallBooleanMethod,
+    CallBooleanMethodV,
+    CallBooleanMethodA,
+    CallByteMethod,
+    CallByteMethodV,
+    CallByteMethodA,
+    CallCharMethod,
+    CallCharMethodV,
+    CallCharMethodA,
+    CallShortMethod,
+    CallShortMethodV,
+    CallShortMethodA,
+    CallIntMethod,
+    CallIntMethodV,
+    CallIntMethodA,
+    CallLongMethod,
+    CallLongMethodV,
+    CallLongMethodA,
+    CallFloatMethod,
+    CallFloatMethodV,
+    CallFloatMethodA,
+    CallDoubleMethod,
+    CallDoubleMethodV,
+    CallDoubleMethodA,
+    CallVoidMethod,
+    CallVoidMethodV,
+    CallVoidMethodA,
+
+    CallNonvirtualObjectMethod,
+    CallNonvirtualObjectMethodV,
+    CallNonvirtualObjectMethodA,
+    CallNonvirtualBooleanMethod,
+    CallNonvirtualBooleanMethodV,
+    CallNonvirtualBooleanMethodA,
+    CallNonvirtualByteMethod,
+    CallNonvirtualByteMethodV,
+    CallNonvirtualByteMethodA,
+    CallNonvirtualCharMethod,
+    CallNonvirtualCharMethodV,
+    CallNonvirtualCharMethodA,
+    CallNonvirtualShortMethod,
+    CallNonvirtualShortMethodV,
+    CallNonvirtualShortMethodA,
+    CallNonvirtualIntMethod,
+    CallNonvirtualIntMethodV,
+    CallNonvirtualIntMethodA,
+    CallNonvirtualLongMethod,
+    CallNonvirtualLongMethodV,
+    CallNonvirtualLongMethodA,
+    CallNonvirtualFloatMethod,
+    CallNonvirtualFloatMethodV,
+    CallNonvirtualFloatMethodA,
+    CallNonvirtualDoubleMethod,
+    CallNonvirtualDoubleMethodV,
+    CallNonvirtualDoubleMethodA,
+    CallNonvirtualVoidMethod,
+    CallNonvirtualVoidMethodV,
+    CallNonvirtualVoidMethodA,
+
+    GetFieldID,
+
+    GetObjectField,
+    GetBooleanField,
+    GetByteField,
+    GetCharField,
+    GetShortField,
+    GetIntField,
+    GetLongField,
+    GetFloatField,
+    GetDoubleField,
+    SetObjectField,
+    SetBooleanField,
+    SetByteField,
+    SetCharField,
+    SetShortField,
+    SetIntField,
+    SetLongField,
+    SetFloatField,
+    SetDoubleField,
+
+    GetStaticMethodID,
+
+    CallStaticObjectMethod,
+    CallStaticObjectMethodV,
+    CallStaticObjectMethodA,
+    CallStaticBooleanMethod,
+    CallStaticBooleanMethodV,
+    CallStaticBooleanMethodA,
+    CallStaticByteMethod,
+    CallStaticByteMethodV,
+    CallStaticByteMethodA,
+    CallStaticCharMethod,
+    CallStaticCharMethodV,
+    CallStaticCharMethodA,
+    CallStaticShortMethod,
+    CallStaticShortMethodV,
+    CallStaticShortMethodA,
+    CallStaticIntMethod,
+    CallStaticIntMethodV,
+    CallStaticIntMethodA,
+    CallStaticLongMethod,
+    CallStaticLongMethodV,
+    CallStaticLongMethodA,
+    CallStaticFloatMethod,
+    CallStaticFloatMethodV,
+    CallStaticFloatMethodA,
+    CallStaticDoubleMethod,
+    CallStaticDoubleMethodV,
+    CallStaticDoubleMethodA,
+    CallStaticVoidMethod,
+    CallStaticVoidMethodV,
+    CallStaticVoidMethodA,
+
+    GetStaticFieldID,
+
+    GetStaticObjectField,
+    GetStaticBooleanField,
+    GetStaticByteField,
+    GetStaticCharField,
+    GetStaticShortField,
+    GetStaticIntField,
+    GetStaticLongField,
+    GetStaticFloatField,
+    GetStaticDoubleField,
+
+    SetStaticObjectField,
+    SetStaticBooleanField,
+    SetStaticByteField,
+    SetStaticCharField,
+    SetStaticShortField,
+    SetStaticIntField,
+    SetStaticLongField,
+    SetStaticFloatField,
+    SetStaticDoubleField,
+
+    NewString,
+
+    GetStringLength,
+    GetStringChars,
+    ReleaseStringChars,
+
+    NewStringUTF,
+    GetStringUTFLength,
+    GetStringUTFChars,
+    ReleaseStringUTFChars,
+
+    GetArrayLength,
+
+    NewObjectArray,
+    GetObjectArrayElement,
+    SetObjectArrayElement,
+
+    NewBooleanArray,
+    NewByteArray,
+    NewCharArray,
+    NewShortArray,
+    NewIntArray,
+    NewLongArray,
+    NewFloatArray,
+    NewDoubleArray,
+
+    GetBooleanArrayElements,
+    GetByteArrayElements,
+    GetCharArrayElements,
+    GetShortArrayElements,
+    GetIntArrayElements,
+    GetLongArrayElements,
+    GetFloatArrayElements,
+    GetDoubleArrayElements,
+
+    ReleaseBooleanArrayElements,
+    ReleaseByteArrayElements,
+    ReleaseCharArrayElements,
+    ReleaseShortArrayElements,
+    ReleaseIntArrayElements,
+    ReleaseLongArrayElements,
+    ReleaseFloatArrayElements,
+    ReleaseDoubleArrayElements,
+
+    GetBooleanArrayRegion,
+    GetByteArrayRegion,
+    GetCharArrayRegion,
+    GetShortArrayRegion,
+    GetIntArrayRegion,
+    GetLongArrayRegion,
+    GetFloatArrayRegion,
+    GetDoubleArrayRegion,
+    SetBooleanArrayRegion,
+    SetByteArrayRegion,
+    SetCharArrayRegion,
+    SetShortArrayRegion,
+    SetIntArrayRegion,
+    SetLongArrayRegion,
+    SetFloatArrayRegion,
+    SetDoubleArrayRegion,
+
+    RegisterNatives,
+    UnregisterNatives,
+
+    MonitorEnter,
+    MonitorExit,
+
+    GetJavaVM,
+
+    GetStringRegion,
+    GetStringUTFRegion,
+
+    GetPrimitiveArrayCritical,
+    ReleasePrimitiveArrayCritical,
+
+    GetStringCritical,
+    ReleaseStringCritical,
+
+    NewWeakGlobalRef,
+    DeleteWeakGlobalRef,
+
+    ExceptionCheck,
+
+    NewDirectByteBuffer,
+    GetDirectBufferAddress,
+    GetDirectBufferCapacity,
+
+    GetObjectRefType,
 }
