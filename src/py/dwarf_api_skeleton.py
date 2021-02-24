bf8c37bfc78a4e270e2252b7edf1b0c084c16918
PinkiePieStyle <svenw1980@gmail.com>
2021-02-24 12:13:02 +0100

    sync

--- src/py/dwarf_api_skeleton.py
+++ src/py/dwarf_api_skeleton.py
@@ -1,5 +1,5 @@
 """
-    Dwarf - Copyright (C) 2018-2020 Giovanni Rocca (iGio90)
+    Dwarf - Copyright (C) 2018-2021 Giovanni Rocca (iGio90)
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -12,8 +12,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.
-    If not, see <https://www.gnu.org/licenses/>
+    along with this program. If not, see <https://www.gnu.org/licenses/>
 """
 
 from PyQt5.QtCore import QObject
